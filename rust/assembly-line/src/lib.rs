db8bd694db440d04c75111965f6aefdc25392913
ixomaxip <ixomaxip@gmail.com>
2021-12-16 00:01:21 +0500

    submit assembly-line

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -3,9 +3,18 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    
+    let rate = speed as f64 * 221.0;
+    match speed {
+        1..=4 => rate,
+        5..=8 => 0.9 * rate,
+        9..=10 => 0.77 * rate,
+        _ =>  0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    
+    let rate_per_minute = production_rate_per_hour(speed) / 60.0;
+    return rate_per_minute as u32
 }
