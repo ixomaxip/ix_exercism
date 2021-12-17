2f60a48c8b933c9048c571969862e58baf2c01c6
ixomaxip <ixomaxip@gmail.com>
2021-12-18 01:03:50 +0500

    submit semi-structured-logs

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -8,17 +8,27 @@ pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    match level {
+        LogLevel::Info => info(message),
+        LogLevel::Warning => warn(message),
+        LogLevel::Error => error(message),
+        LogLevel::Debug => debug(message),
+    }
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    format!("[INFO]: {}", message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    format!("[WARNING]: {}", message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    format!("[ERROR]: {}", message)
 }
+pub fn debug(message: &str) -> String {
+    format!("[DEBUG]: {}", message)
+}
+
