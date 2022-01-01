7824df3b6baebfd8f0d94707b52fc69ced1024bd
ixomaxip <ixomaxip@gmail.com>
2022-01-02 00:18:59 +0500

    submit Magazine Cutout

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,15 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut freqs: HashMap<&str, u32> = HashMap::new();
+    for word in magazine {
+        *freqs.entry(word).or_insert(0) += 1;
+    }
+    for word in note {
+        match freqs.get_mut(word) {
+            Some(0) | None => return false,
+            Some(v) => *v -= 1,
+        }
+    }
+    return true;
 }
