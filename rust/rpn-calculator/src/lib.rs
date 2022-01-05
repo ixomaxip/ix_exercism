363639cd61df05a1387d264649b991a749a840a7
ixomaxip <ixomaxip@gmail.com>
2022-01-06 01:11:08 +0500

    submit rpn-calculator

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -7,9 +7,34 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
+fn calc_result(stack: &mut Vec<Option<i32>>, operator: &CalculatorInput) -> Option<i32> {
+    match (stack.pop().flatten(), stack.pop().flatten()) {
+        (Some(b), Some(a)) => {
+            return match operator {
+                    CalculatorInput::Multiply => Some(a * b),
+                    CalculatorInput::Divide => Some(a / b),
+                    CalculatorInput::Add => Some(a + b),
+                    CalculatorInput::Subtract => Some(a - b),
+                    _ => None
+                };
+            },
+        _ => return None
+    };
+}
+
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut stack: Vec<Option<i32>> = vec![];
+
+    for item in inputs {
+        let result = match item {
+            CalculatorInput::Value(n) => Some(*n),
+            _ => calc_result(&mut stack, item)
+        };
+        stack.push(result);
+    }
+
+    match stack.len() {
+        1 => stack.pop().flatten(),
+        _ => None
+    }    
 }
