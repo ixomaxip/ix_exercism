61e69ff8eac07842ef5ab5639626282a3db98585
ixomaxip <ixomaxip@gmail.com>
2022-01-08 01:37:46 +0500

    tests passed up to test_negative_minutes_roll_over

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -12,14 +12,31 @@ impl fmt::Display for Clock {
 }
 
 impl Clock {
+    fn correct_time(hours: i32, minutes: i32) -> (i32, i32) {
+        let hours = ((hours % 24) + 24) % 24;
+        let whole_minutes = hours * 60 + minutes;
+        println!("whole: {:?}", whole_minutes);
+        let hours = whole_minutes / 60 % 24;
+        let hours = ((hours % 24) + 24) % 24;
+        
+        // let minutes = whole_minutes % 60;
+        let minutes = ((whole_minutes % 60) + 60) % 60;
+
+        // println!("{:?}", (minutes / 60));
+        (hours, minutes)
+    }
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let clock = Clock {hours: hours,
-                           minutes: minutes};
+        let correct_time = Clock::correct_time(hours, minutes);
+        // let minutes = minutes % 60;
+        let clock = Clock {hours: correct_time.0,
+                           minutes: correct_time.1};
         clock
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Clock {hours: self.hours + minutes / 60,
-               minutes: self.minutes + minutes % 60}
+        let hours = self.hours + minutes / 60;
+        let minutes = self.minutes + minutes % 60;
+        Clock {hours: hours % 24,
+               minutes: minutes % 60}
     }
 }
