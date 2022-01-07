b6cca521315af5f801e9ccbb1002ac862437d54f
ixomaxip <ixomaxip@gmail.com>
2022-01-07 18:24:22 +0500

    submit armstrong-numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+
+    let mut n = num;
+    let mut digits = vec![]; 
+
+    loop {
+        digits.push(n % 10);
+        n /= 10;
+        if n == 0 {
+            break;
+        }
+    }
+    let l = digits.len() as u32;
+    let sum = digits
+        .iter()
+        .map(|x| x.pow(l))
+        .sum();
+    
+    num == sum
 }
