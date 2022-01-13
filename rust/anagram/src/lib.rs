70d234a43013aadcbffe0e5fdea2346d0dd4d48a
ixomaxip <ixomaxip@gmail.com>
2022-01-14 02:24:21 +0500

    submit

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,27 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let result: HashSet<&str> =
         possible_anagrams
-    );
+            .iter().cloned()
+            .filter(|&s| s.to_lowercase() != word.to_lowercase())
+            .filter(|&s| s.len() == word.len())
+            .filter(|&s| {
+                            let mut word = word
+                                .to_lowercase()
+                                .chars()
+                                .fold(HashMap::new(), |mut map, c| {
+                                    *map.entry(c).or_insert(0) += 1;
+                                    map
+                                });
+            
+                            for c in s.to_lowercase().chars() {
+                                word.entry(c).and_modify(|v| {*v -= 1});
+                            }
+                            word.values().all(|&x| x ==0)
+                        }
+                    )
+            .collect();
+    result
 }
