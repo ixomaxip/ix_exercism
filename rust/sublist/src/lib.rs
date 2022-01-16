f7c9eb40032184f9c44d97fe98d1b0a32b869105
ixomaxip <ixomaxip@gmail.com>
2022-01-16 18:37:31 +0500

    submit

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -6,6 +8,21 @@ pub enum Comparison {
     Unequal,
 }
 
+fn superlist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> bool {
+    _second_list.is_empty() 
+    || _first_list
+            .windows(_second_list.len())
+            .any(|sub| sub == _second_list)
+}
+
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    match _first_list.len().cmp(&_second_list.len()) {
+        Ordering::Greater
+            if superlist(_first_list, _second_list) => Comparison::Superlist,
+        Ordering::Less
+            if superlist(_second_list, _first_list) => Comparison::Sublist,
+        Ordering::Equal
+            if _first_list == _second_list => Comparison::Equal,
+        _ => Comparison::Unequal
+    }
 }
