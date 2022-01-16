b4503f1fc1589b862875477bd48d8c2722538e6e
ixomaxip <ixomaxip@gmail.com>
2022-01-16 19:14:09 +0500

    submit

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,14 +1,15 @@
+fn sum(n : u32) -> u32 {
+    n * (n + 1) / 2
+}
+
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{}", n)
+    sum(n).pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{}", n)
+    sum(n) * (2 * n + 1) / 3
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!(
-        "difference between square of sum of 1...{n} and sum of squares of 1...{n}",
-        n = n,
-    )
+    square_of_sum(n) - sum_of_squares(n)
 }
