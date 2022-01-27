64a8285045e98cc798ec7b39e2579772b34ecc9e
ixomaxip <ixomaxip@gmail.com>
2022-01-28 01:03:57 +0500

    flatten & fill

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,5 +1,18 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let res: Vec<String> = vec!();
 
+    let mut v = minefield
+        .concat()
+        .chars()
+        .map(|ch| match ch {
+            '*' => -1,
+            _ => 0
+        })
+        .collect::<Vec<_>>();
+
+    let dim = Dim {
+        n: minefield.len(),
+        m: v.len() / minefield.len()
+    };
     res
 }
