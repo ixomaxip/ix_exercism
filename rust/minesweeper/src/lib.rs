d701925c4ae1f7d0079246a6f2c2b44e878a5f35
ixomaxip <ixomaxip@gmail.com>
2022-02-12 15:49:13 +0500

    get surrounds

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,43 @@
+
+fn get_idxs(idx: usize, n: usize, m: usize) -> Vec<usize> {
+    
+    let res: Vec<usize> = vec![];
+    //    <------m------>     
+    // -    -    -    -    -  |
+    // -    ul   u    ur   -  n
+    // -    l   idx   r    -  |
+    // -    dl   d   dr    -  |
+
+
+    // -    -    -    -    -
+
+    let m = m as i32;
+    let n = n as i32;
+
+    // let is_valid = |i: i32| -> bool {        
+
+    // };
+
+    let i = idx as i32;
+    let l: i32 = match ((i - 1) / m).cmp(&(i / m)) {
+        Ordering::Equal => i - 1,
+        _ => -1
+    };
+    let r: i32 = match ((i + 1) / m).cmp(&(i / m)) {
+        Ordering::Equal => i + 1,
+        _ => -1
+    };
+
+    let u: i32 = i - m;
+    let ul: i32 = u - 1;
+    let ur: i32 = u + 1;
+
+    let d: i32 = i + m;
+    let dl: i32 = d - 1;
+    let dr: i32 = d + 1;
+
+    res
+}
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
     let res: Vec<String> = vec!();
 
