65189d84c9eec0a6a31a2e5bf1f955ae8db55fd4
ixomaxip <ixomaxip@gmail.com>
2022-02-18 22:19:42 +0500

    submit prime-factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut primes: Vec<u64> = vec![];
+
+    let mut nn = n;
+    while nn % 2 == 0 {
+        primes.push(2);
+        nn /= 2;
+    }
+    let upper = (nn as f64).sqrt() as u64 + 1;
+    for i in (3..=upper).step_by(2) {
+        while nn % i == 0 {
+            primes.push(i);
+            nn /= i;
+        }
+    }
+    if nn > 2 { primes.push(nn)}
+    primes
 }
