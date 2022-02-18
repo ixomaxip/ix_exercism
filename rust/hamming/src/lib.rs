cd83eb4105a74b08ed2f41aa2e28c5f692bef4a3
ixomaxip <ixomaxip@gmail.com>
2022-02-19 00:19:41 +0500

    submit hamming

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,5 +1,14 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    unimplemented!("What is the Hamming Distance between {} and {}", s1, s2);
+    
+    match s1.len() == s2.len() {
+        true => Some(s1.chars()
+            .zip(s2.chars())
+            .filter(|(x, y)| x != y)
+            .collect::<Vec<_>>()
+            .len()
+        ),
+        false => None,
+    }
 }
