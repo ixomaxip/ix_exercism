85d42ea4c58c4b15d85b640ac2490ceb84834b72
ixomaxip <ixomaxip@gmail.com>
2022-02-25 00:59:57 +0500

    renaming

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -9,41 +9,33 @@ fn split(cols: usize) -> impl Fn(usize) -> (usize, usize) {
 }
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let res: Vec<String> = vec!();
-    let rows = minefield.len() + 2;
-    let cols = minefield[0].len() + 2;
+    let rows = minefield.len();
+    let cols = minefield[0].len();
 
-    let mut field = vec![0i32; (cols - 2) * (rows - 2)];
-    let mut padded_field = vec![0i32; cols* rows];
+    let mut field = vec![" ".to_string(); cols * rows];
+
+    let window_size = (3usize, 3usize);    
+    let padded_rows = rows + window_size.0 - 1;
+    let padded_cols = cols + window_size.1 - 1;
+
+    let mut padded_field = vec![0i32; padded_rows * padded_cols];
     for (row, s) in minefield.iter().enumerate() {
         for (col, ch) in s.chars().enumerate() {
             if ch == '*' {
-                padded_field[(row + 1) * cols + col + 1] = -1;
-                field[row * (cols - 2) + col] = -1;
+                padded_field[(row + 1) * padded_cols + col + 1] = -1;
+                field[row * cols + col] = ch.to_string();
             }
         }
     }
 
-    // for debug 
-    let v = field
-        .chunks(cols - 2)
-        .collect::<Vec<_>>();
-
-
-    let window_size = (3usize, 3usize);
-    let trimmed_field = (
-        rows - window_size.0 + 1,
-        cols - window_size.1 + 1
-    );
-
-    let win_iter = (0..trimmed_field.0 * trimmed_field.1)
-        .map(split(trimmed_field.1))
+    let win_iter = (0..rows * cols)
+        .map(split(cols))
         .map(|(row, col)| {
             let borrowed_field = &padded_field;
             (0..window_size.0 * window_size.1)
                 .map(split(window_size.1))
                 .map(move |(wrow, wcol)| {
-                    let idx = (wrow + row) * cols + (wcol + col);
+                    let idx = (wrow + row) * padded_cols + (wcol + col);
                     unsafe { *borrowed_field.get_unchecked(idx)}
                 })
             });
