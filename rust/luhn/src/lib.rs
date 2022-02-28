2d78161f2b72569b0ab42a3a9126d1b35841f0cb
ixomaxip <ixomaxip@gmail.com>
2022-03-01 00:15:01 +0500

    naive impl

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,4 +1,23 @@
+fn calc(idx: usize, val: u32) -> u32 {
+    match idx % 2 {
+        0 => {
+            let doubled = 2 * val;
+            if doubled > 9 { doubled - 9 } else { doubled }
+        },
+        _ => val
+    }
+}
+
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let res: u32 = code
+        .chars()
+        .filter_map(|c| c.to_digit(10))
+        .enumerate()
+        .map(|(idx, val)| calc(idx, val))
+        .sum();
+
+    println!("{:?}", res);
+
+    res % 10 == 0
 }
