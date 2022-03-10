91075f8f0971f03380f5a106d4954440f8569c91
ixomaxip <ixomaxip@gmail.com>
2022-03-10 20:19:57 +0500

    better solution

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -44,3 +44,15 @@ pub fn is_valid(code: &str) -> bool {
 
     res % 10 == 0
 }
+
+pub fn is_valid2(code: &str) -> bool {
+    code.chars()
+        .rev()
+        .filter(|c| !c.is_whitespace())
+        .try_fold((0, 0), |(sum, count), val| {
+            val.to_digit(10)
+                .map(|num| if count % 2 == 1 { num * 2 } else { num })
+                .map(|num| if num > 9 { num - 9 } else { num })
+                .map(|num| (num + sum, count + 1))
+        }).map_or(false, |(sum, count)| sum % 10 == 0 && count > 1)
+}
