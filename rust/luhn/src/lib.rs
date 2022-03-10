c53de3ee5f3bfbef241cbb19ef969e13bb8b2764
ixomaxip <ixomaxip@gmail.com>
2022-03-10 20:17:49 +0500

    submit luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,23 +1,46 @@
 fn calc(idx: usize, val: u32) -> u32 {
-    match idx % 2 {
-        0 => {
+    match idx & 1 {
+        1 => {
             let doubled = 2 * val;
-            if doubled > 9 { doubled - 9 } else { doubled }
-        },
-        _ => val
+            if doubled > 9 {
+                doubled - 9
+            } else {
+                doubled
+            }
+        }
+        _ => val,
     }
 }
 
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let res: u32 = code
+    let trimmed = code
         .chars()
-        .filter_map(|c| c.to_digit(10))
-        .enumerate()
-        .map(|(idx, val)| calc(idx, val))
-        .sum();
+        .filter(|c| c.to_string() != " ")
+        .collect::<Vec<_>>();
 
-    println!("{:?}", res);
+    if trimmed.len() <= 1 {
+        return false;
+    }
+
+    let (numbers, errors): (Vec<_>, Vec<_>) = trimmed
+        .iter()
+        .map(|c| c.to_string().parse::<u32>())
+        .partition(Result::is_ok);
+
+    if errors.len() != 0 {
+        return false;
+    }
+
+    let res: u32 = numbers
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(idx, val)| match val {
+            Ok(val) => calc(idx, *val),
+            Err(_) => unreachable!(),
+        })
+        .sum();
 
     res % 10 == 0
 }
