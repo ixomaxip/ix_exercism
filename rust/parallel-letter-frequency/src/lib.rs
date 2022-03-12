822b9e78a5a0acae7eb90687eff63cee813f5410
ixomaxip <ixomaxip@gmail.com>
2022-03-12 10:05:01 +0500

    empty map

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -1,12 +1,18 @@
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    unimplemented!(
-        "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
-        input,
-        match worker_count {
-            1 => "1 worker".to_string(),
-            _ => format!("{} workers", worker_count),
-        }
-    );
+    // unimplemented!(
+    //     "Count the frequency of letters in the given input '{:?}'. Ensure that you are using {} to process the input.",
+    //     input,
+    //     match worker_count {
+    //         1 => "1 worker".to_string(),
+    //         _ => format!("{} workers", worker_count),
+    //     }
+    // );
+    // let counters = Mutex<>::new
+
+    let result = HashMap::<char, usize>::new();
+
+    return result;
 }
