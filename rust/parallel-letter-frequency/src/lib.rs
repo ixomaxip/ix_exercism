dda4c8dd11952f3f06f69e4c7b0a33f8000a2698
ixomaxip <ixomaxip@gmail.com>
2022-03-19 12:01:16 +0500

    refac

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -2,8 +2,17 @@ use std::collections::HashMap;
 use std::sync::Mutex;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let input = input.join("");
-    let result = worker(&input);
+    let input = input.join("")
+        .to_lowercase()
+        .replace(&['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][..], "")
+        .replace(&['.', ',', '!', '?', ';', ':', ')', '(', '\'', '\"'][..], "");
+
+    let result = input
+        .chars()
+        .fold(HashMap::new(), |mut map, c| {
+            *map.entry(c).or_insert(0) += 1;
+            map
+        });
     return result;
 }
 
@@ -12,7 +21,6 @@ fn worker(data: &str) -> HashMap<char, usize> {
         .to_lowercase()
         .replace(&['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'][..], "")
         .replace(&['.', ',', '!', '?', ';', ':', ')', '(', '\'', '\"'][..], "")
-
         .chars()
         .fold(HashMap::new(), |mut map, c| {
             *map.entry(c).or_insert(0) += 1;
