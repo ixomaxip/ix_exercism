f6c7d8140467616c3055b22d8ec025e11fefca50
ixomaxip <ixomaxip@gmail.com>
2022-04-22 00:51:15 +0500

    submit proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,11 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    match list.len() {
+        0 => String::new(),
+        _ => format!("{1}And all for the want of a {0}.",
+            list[0],
+            list.windows(2).fold(String::new(), |acc, w| {
+                format!("{}For want of a {} the {} was lost.\n", acc, w[0], w[1])
+            })
+        )
+    }
 }
