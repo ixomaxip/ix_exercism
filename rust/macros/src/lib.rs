a94c99acbaaddc31561a6b8ad1cf7fa0a78d138e
ixomaxip <ixomaxip@gmail.com>
2022-05-03 19:01:29 +0500

    refac macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,13 +1,8 @@
 #[macro_export]
 macro_rules! hashmap {
-    ( $($k:expr => $v:expr),* ) => {{
-        let mut h_map = crate::HashMap::new();
-        $(
-            h_map.insert($k, $v);
-        )*
+    ( $( $($k:expr => $v:expr)+ $(,)? )* ) => {{
+        let mut h_map = ::std::collections::HashMap::new();
+        $( $( h_map.insert($k, $v); )* )*
         h_map
     }};
-    ( $($k:expr => $v:expr,)* ) => {{
-        crate::hashmap!($($k => $v),*)
-    }}
 }
