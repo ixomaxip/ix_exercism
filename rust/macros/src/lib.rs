b903df12e5a60cc3ad1092c510992ed525e78adb
ixomaxip <ixomaxip@gmail.com>
2022-05-03 18:40:43 +0500

    submit macros

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,10 +1,13 @@
 #[macro_export]
 macro_rules! hashmap {
-    ( $($k:expr => $v:expr $(,)?)* ) => {{
+    ( $($k:expr => $v:expr),* ) => {{
         let mut h_map = crate::HashMap::new();
         $(
             h_map.insert($k, $v);
         )*
         h_map
     }};
+    ( $($k:expr => $v:expr,)* ) => {{
+        crate::hashmap!($($k => $v),*)
+    }}
 }
