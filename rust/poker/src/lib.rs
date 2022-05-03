f9a281746d941e2f4c012b22bcb8bea0f6e0d929
ixomaxip <ixomaxip@gmail.com>
2022-05-03 21:14:19 +0500

    add basic enums & structs

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,3 +1,46 @@
+
+#[derive(Debug)]
+enum Value {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
+
+impl Value {
+    fn new(value: &str) -> Value {
+        match value {
+            "A" => Value::ACE,
+            "2" => Value::TWO,
+            "3" => Value::THREE,
+            "4" => Value::FOUR,
+            "5" => Value::FIVE,
+            "6" => Value::SIX,
+            "7" => Value::SEVEN,
+            "8" => Value::EIGHT,
+            "9" => Value::NINE,
+            "10" => Value::TEN,
+            "J" => Value::JACK,
+            "Q" => Value::QUEEN,
+            "K" => Value::KING,
+            _ => unreachable!("Invalid card value")
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Suit {
+    DIAMONDS, CLUBS, HEARTS, SPADES
+}
+
+#[derive(Debug)]
+struct Card {
+    value: Value,
+    suit: Suit,
+}
+
+#[derive(Debug)]
+struct PokerHand {
+    cards: [Card; 5]
+}
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
