c16842a4a794eb2cb8e192c0e7c7ac54321a4267
ixomaxip <ixomaxip@gmail.com>
2022-05-03 18:04:44 +0500

    fix comma position

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! hashmap {
-    () => {{
-        crate::HashMap::new()
-    }};
-    ( $($k:expr => $v:expr),* $(,)?) => {{
+    // () => {{
+    //     crate::HashMap::new()
+    // }};
+    ( $($k:expr => $v:expr $(,)?)* ) => {{
         let mut h_map = crate::HashMap::new();
         $(
             h_map.insert($k, $v);
