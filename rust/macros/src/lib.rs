c3f69730e8e413d1172e8a4e789fa183aebeaf82
ixomaxip <ixomaxip@gmail.com>
2022-05-03 14:38:26 +0500

    simple cases

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,6 +1,17 @@
 #[macro_export]
 macro_rules! hashmap {
     () => {
-        unimplemented!()
+        {
+            HashMap::new()
+        }
+    };
+    ( $($k:expr => $v:expr), * ) => {
+        {
+            let mut h_map = HashMap::new();
+            $(
+                h_map.insert($k, $v);
+            )*
+            h_map
+        }
     };
 }
