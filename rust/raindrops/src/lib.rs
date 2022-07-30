1d46c67a94e6c4b8366d9ff7c21d4aeba5889e13
ixomaxip <ixomaxip@gmail.com>
2022-07-30 22:45:41 +0500

    submit raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let drops = vec!((3, "Pling"), (5, "Plang"), (7, "Plong"));
+
+    let mut s = drops.iter()
+        .map(|(k, v)| match n % k {
+            0 => v,
+            _ => ""
+        })
+        .fold(String::new(), |a, b| a + b );
+    if s == "" {
+        s = n.to_string();
+    }
+    s
 }
