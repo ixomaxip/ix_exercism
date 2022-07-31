89bf831a9167f975cbf82bdf8f44e19b960c74d2
ixomaxip <ixomaxip@gmail.com>
2022-08-01 00:02:59 +0500

    bob passed all except 3 cases with no letters

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,3 +1,40 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {}", message)
+    let replies = vec!(
+        "Sure.",
+        "Whoa, chill out!",
+        "Calm down, I know what I'm doing!",
+        "Fine. Be that way!",
+        "Whatever."
+
+    );
+
+    let message = message.trim();
+    let yelling = message.chars()
+        .filter(|c| c.is_alphanumeric())
+        .filter(|c| !c.is_numeric())
+        .collect::<String>();
+        
+        
+    let is_question = message.ends_with("?");
+    // let uppercase = (b'A'..b'Z').map(|c| c as char).collect::<String>();
+    // let is_yelling = yelling.chars().all(|c| uppercase.contains(c));
+    let is_yelling = yelling.chars().all(|c| c.is_uppercase());
+    // dbg!(message);
+    // dbg!(yelling);
+    // dbg!(is_yelling);
+    match message == "" {
+        true => replies[3],
+        false => match is_yelling {
+            true => match is_question {
+                true => replies[2],
+                false => replies[1],
+            },
+            false => match is_question {
+                true => replies[0],
+                false => replies[4],
+            }
+        }
+    }
+
+    // replies[4]
 }
--- rust/bob/tests/bob.rs
+++ rust/bob/tests/bob.rs
@@ -9,28 +9,28 @@ fn test_stating_something() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// ending with whitespace
 fn test_ending_with_whitespace() {
     process_response_case("Okay if like my  spacebar  quite a bit?   ", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting numbers
 fn test_shouting_numbers() {
     process_response_case("1, 2, 3 GO!", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// other whitespace
 fn test_other_whitespace() {
     process_response_case("\r\r 	", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting with special characters
 fn test_shouting_with_special_characters() {
     process_response_case(
@@ -40,35 +40,35 @@ fn test_shouting_with_special_characters() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// talking forcefully
 fn test_talking_forcefully() {
     process_response_case("Hi there!", "Whatever.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// prattling on
 fn test_prattling_on() {
     process_response_case("Wait! Hang on. Are you going to be OK?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// forceful question
 fn test_forceful_question() {
     process_response_case("WHAT'S GOING ON?", "Calm down, I know what I'm doing!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting with no exclamation mark
 fn test_shouting_with_no_exclamation_mark() {
     process_response_case("I HATE THE DENTIST", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// asking gibberish
 fn test_asking_gibberish() {
     process_response_case("fffbbcbeab?", "Sure.");
@@ -89,7 +89,7 @@ fn test_no_letters() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// statement containing question mark
 fn test_statement_containing_question_mark() {
     process_response_case("Ending with ? means a question.", "Whatever.");
@@ -97,7 +97,7 @@ fn test_statement_containing_question_mark() {
 
 //NEW
 #[test]
-#[ignore]
+// #[ignore]
 /// multiple line question
 fn test_multiple_line_question() {
     process_response_case(
@@ -107,7 +107,7 @@ fn test_multiple_line_question() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// non-question ending with whitespace
 fn test_nonquestion_ending_with_whitespace() {
     process_response_case(
@@ -117,7 +117,7 @@ fn test_nonquestion_ending_with_whitespace() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting
 fn test_shouting() {
     process_response_case("WATCH OUT!", "Whoa, chill out!");
@@ -131,42 +131,42 @@ fn test_nonletters_with_question() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// shouting gibberish
 fn test_shouting_gibberish() {
     process_response_case("FCECDFCAAB", "Whoa, chill out!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// asking a question
 fn test_asking_a_question() {
     process_response_case("Does this cryogenic chamber make me look fat?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// asking a numeric question
 fn test_asking_a_numeric_question() {
     process_response_case("You are, what, like 15?", "Sure.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// silence
 fn test_silence() {
     process_response_case("", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// starting with whitespace
 fn test_starting_with_whitespace() {
     process_response_case("         hmmmmmmm...", "Whatever.");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// using acronyms in regular speech
 fn test_using_acronyms_in_regular_speech() {
     process_response_case(
@@ -176,14 +176,14 @@ fn test_using_acronyms_in_regular_speech() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// alternate silence
 fn test_alternate_silence() {
     process_response_case("										", "Fine. Be that way!");
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 /// prolonged silence
 fn test_prolonged_silence() {
     process_response_case("          ", "Fine. Be that way!");
