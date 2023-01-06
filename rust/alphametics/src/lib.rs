095eb135441d8d19b13b10b6a7bef4757a16dfbd
ixomaxip <ixomaxip@gmail.com>
2023-01-06 23:18:01 +0100

    split to words & get letters

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,5 +1,26 @@
+#[warn(unused_imports)]
 use std::collections::HashMap;
+use regex::Regex;
+use itertools::Itertools;
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
-    unimplemented!("Solve the alphametic {:?}", input)
+    let sep = Regex::new(r".\+.|.==.");
+    let words: Vec<_> = sep
+        .unwrap()
+        .split(input)
+        .into_iter()
+        .collect();
+    println!("words: {:?}", words);
+    let letters: Vec<char> = words.join("").chars().unique().sorted().collect();
+    
+    // let letters = input
+    //     .chars()
+    //     .filter(|c| c.is_alphanumeric())
+    //     .fold(HashMap::new(), | mut map, c| {
+        //         *map.entry(c).or_insert(0);
+        //         map
+        //     });
+        
+
+    Some(HashMap::new())
 }
